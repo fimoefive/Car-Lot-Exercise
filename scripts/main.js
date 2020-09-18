ab3c089dbd501803b8199dce3ed99344c0c20a9a
fimoefive <brothermj@hotmail.com>
2020-09-18 16:20:54 -0500

    main.js update

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,7 @@
 import { CarList} from "./Cars/CarsList.js";
-import { getCars, useCars } from "./Cars/CarsProvider.js";
 import { ColorList } from "./Colors/ColorsList.js";
-import { getAvailableColors, useColors } from "./Colors/ColorsProvider.js";
+
 
 
 CarList();
 ColorList();
-getCars();
-useCars();
-getAvailableColors();
-useColors();
