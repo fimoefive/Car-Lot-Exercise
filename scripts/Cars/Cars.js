605efa10a384136548b33c9fbd79be1e2f29b527
fimoefive <brothermj@hotmail.com>
2020-09-18 11:46:55 -0500

    Colors.js and Cars.js files

---
+++ scripts/Cars/Cars.js
@@ -0,0 +1,6 @@
+export const carName = (eateryObj) =>{
+    return ` 
+    <section id="car-card"${carObj.id} class="car-card">
+    <h2>${carObj.businessName}</h2>
+    ` 
+};
---
+++ scripts/Colors/Colors.js
@@ -0,0 +1,6 @@
+export const colorName = (colorObj) =>{
+    return ` 
+    <section id="color-card"${colorObj.id} class="color-card">
+    <h2>${colorObj.colorName}</h2>
+    `  
+};
